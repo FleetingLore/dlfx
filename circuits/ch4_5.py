b59ac2c1c0e258d74f6c0d4eeb0062f75d8ea41b
石山灰 <Macro__9@outlook.com>
2026-05-21 19:59:23 +0800

    Update ch4_5.py

--- circuits/ch4_5.py
+++ circuits/ch4_5.py
@@ -14,16 +14,16 @@ STYLE = {"fontsize": 14}
 with Drawing(show=False) as d:
     d.config(fontsize=STYLE["fontsize"])
 
-    d += elm.Dot(open=True).at((3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((3, 0)).label("$+$", "right")
     d += elm.Line().at((3, 0)).to((0, 0))
 
     d += elm.SourceV().at((0, -2)).to((0, 0)).label("$u_{S1}$")
     d += elm.SourceV().at((0, -4)).to((0, -2)).label("$u_{S2}$")
 
     d += elm.Line().at((0, -4)).to((3, -4))
-    d += elm.Dot(open=True).at((3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((1, 0)).to((2, 0)).label("i", "top")
+    d += elm.Arrow().at((1, 0)).to((2, 0)).label("$i$", "top")
 
     d += (
         elm.Line().at((-1.5, +0.5)).to((+1.0, +0.5)).linestyle("dashdot").linewidth(0.5)
@@ -38,19 +38,19 @@ with Drawing(show=False) as d:
         elm.Line().at((-1.5, -4.5)).to((-1.5, +0.5)).linestyle("dashdot").linewidth(0.5)
     )
 
-    d += elm.Line().at((3, -2)).to((3, -2)).label("u", "center")
+    d += elm.Line().at((3, -2)).to((3, -2)).label("$u$", "center")
 
     base = 6
 
-    d += elm.Dot(open=True).at((base + 3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((base + 3, 0)).label("$+$", "right")
     d += elm.Line().at((base + 3, 0)).to((base + 0, 0))
 
-    d += elm.SourceV().at((base + 0, -4)).to((base + 0, 0)).label("u[S]")
+    d += elm.SourceV().at((base + 0, -4)).to((base + 0, 0)).label("$u_S$")
 
     d += elm.Line().at((base + 0, -4)).to((base + 3, -4))
-    d += elm.Dot(open=True).at((base + 3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((base + 3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("i", "top")
+    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("$i$", "top")
 
     d += (
         elm.Line()
@@ -81,7 +81,7 @@ with Drawing(show=False) as d:
         .linewidth(0.5)
     )
 
-    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("u", "center")
+    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("$u$", "center")
 
     d.save(OUTPUT_DIR / "4_5_1.svg")
 
@@ -89,19 +89,19 @@ with Drawing(show=False) as d:
 with Drawing(show=False) as d:
     d.config(fontsize=STYLE["fontsize"])
 
-    d += elm.Dot(open=True).at((5, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((5, 0)).label("$+$", "right")
     d += elm.Line().at((5, 0)).to((0, 0))
 
-    d += elm.SourceV().at((0, -4)).to((0, 0)).label("u[S]")
+    d += elm.SourceV().at((0, -4)).to((0, 0)).label("$u_S$")
 
     d += elm.Dot(open=False).at((2, 0))
-    d += elm.SourceV().at((2, -4)).to((2, 0)).label("u[S]")
+    d += elm.SourceV().at((2, -4)).to((2, 0)).label("$u_S$")
     d += elm.Dot(open=False).at((2, -4))
 
     d += elm.Line().at((0, -4)).to((5, -4))
-    d += elm.Dot(open=True).at((5, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((5, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((3, 0)).to((4, 0)).label("i", "top")
+    d += elm.Arrow().at((3, 0)).to((4, 0)).label("$i$", "top")
 
     d += (
         elm.Line().at((-1.5, +0.5)).to((+3.0, +0.5)).linestyle("dashdot").linewidth(0.5)
@@ -116,19 +116,19 @@ with Drawing(show=False) as d:
         elm.Line().at((-1.5, -4.5)).to((-1.5, +0.5)).linestyle("dashdot").linewidth(0.5)
     )
 
-    d += elm.Line().at((5, -2)).to((5, -2)).label("u", "center")
+    d += elm.Line().at((5, -2)).to((5, -2)).label("$u$", "center")
 
     base = 8
 
-    d += elm.Dot(open=True).at((base + 3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((base + 3, 0)).label("$+$", "right")
     d += elm.Line().at((base + 3, 0)).to((base + 0, 0))
 
-    d += elm.SourceV().at((base + 0, -4)).to((base + 0, 0)).label("u[S]")
+    d += elm.SourceV().at((base + 0, -4)).to((base + 0, 0)).label("$u_S$")
 
     d += elm.Line().at((base + 0, -4)).to((base + 3, -4))
-    d += elm.Dot(open=True).at((base + 3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((base + 3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("i", "top")
+    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("$i$", "top")
 
     d += (
         elm.Line()
@@ -159,7 +159,7 @@ with Drawing(show=False) as d:
         .linewidth(0.5)
     )
 
-    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("u", "center")
+    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("$u$", "center")
 
     d.save(OUTPUT_DIR / "4_5_2.svg")
 
@@ -167,19 +167,19 @@ with Drawing(show=False) as d:
 with Drawing(show=False) as d:
     d.config(fontsize=STYLE["fontsize"])
 
-    d += elm.Dot(open=True).at((5, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((5, 0)).label("$+$", "right")
     d += elm.Line().at((5, 0)).to((0, 0))
 
-    d += elm.SourceI().at((0, -4)).to((0, 0)).label("i[S]")
+    d += elm.SourceI().at((0, -4)).to((0, 0)).label("$i_S$")
 
     d += elm.Dot(open=False).at((2, 0))
-    d += elm.SourceI().at((2, -4)).to((2, 0)).label("i[S]")
+    d += elm.SourceI().at((2, -4)).to((2, 0)).label("$i_S$")
     d += elm.Dot(open=False).at((2, -4))
 
     d += elm.Line().at((0, -4)).to((5, -4))
-    d += elm.Dot(open=True).at((5, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((5, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((3, 0)).to((4, 0)).label("i", "top")
+    d += elm.Arrow().at((3, 0)).to((4, 0)).label("$i$", "top")
 
     d += (
         elm.Line().at((-1.5, +0.5)).to((+3.0, +0.5)).linestyle("dashdot").linewidth(0.5)
@@ -194,19 +194,19 @@ with Drawing(show=False) as d:
         elm.Line().at((-1.5, -4.5)).to((-1.5, +0.5)).linestyle("dashdot").linewidth(0.5)
     )
 
-    d += elm.Line().at((5, -2)).to((5, -2)).label("u", "center")
+    d += elm.Line().at((5, -2)).to((5, -2)).label("$u$", "center")
 
     base = 8
 
-    d += elm.Dot(open=True).at((base + 3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((base + 3, 0)).label("$+$", "right")
     d += elm.Line().at((base + 3, 0)).to((base + 0, 0))
 
-    d += elm.SourceI().at((base + 0, -4)).to((base + 0, 0)).label("i[S]")
+    d += elm.SourceI().at((base + 0, -4)).to((base + 0, 0)).label("$i_S$")
 
     d += elm.Line().at((base + 0, -4)).to((base + 3, -4))
-    d += elm.Dot(open=True).at((base + 3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((base + 3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("i", "top")
+    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("$i$", "top")
 
     d += (
         elm.Line()
@@ -237,7 +237,7 @@ with Drawing(show=False) as d:
         .linewidth(0.5)
     )
 
-    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("u", "center")
+    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("$u$", "center")
 
     d.save(OUTPUT_DIR / "4_5_3.svg")
 
@@ -245,16 +245,16 @@ with Drawing(show=False) as d:
 with Drawing(show=False) as d:
     d.config(fontsize=STYLE["fontsize"])
 
-    d += elm.Dot(open=True).at((3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((3, 0)).label("$+$", "right")
     d += elm.Line().at((3, 0)).to((0, 0))
 
-    d += elm.SourceI().at((0, -2)).to((0, 0)).label("i[S]")
-    d += elm.SourceI().at((0, -4)).to((0, -2)).label("i[S]")
+    d += elm.SourceI().at((0, -2)).to((0, 0)).label("$i_｛S1}$")
+    d += elm.SourceI().at((0, -4)).to((0, -2)).label("$i_｛S2}$")
 
     d += elm.Line().at((0, -4)).to((3, -4))
-    d += elm.Dot(open=True).at((3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((1, 0)).to((2, 0)).label("i", "top")
+    d += elm.Arrow().at((1, 0)).to((2, 0)).label("$i$", "top")
 
     d += (
         elm.Line().at((-1.5, +0.5)).to((+1.0, +0.5)).linestyle("dashdot").linewidth(0.5)
@@ -269,19 +269,19 @@ with Drawing(show=False) as d:
         elm.Line().at((-1.5, -4.5)).to((-1.5, +0.5)).linestyle("dashdot").linewidth(0.5)
     )
 
-    d += elm.Line().at((3, -2)).to((3, -2)).label("u", "center")
+    d += elm.Line().at((3, -2)).to((3, -2)).label("$u$", "center")
 
     base = 6
 
-    d += elm.Dot(open=True).at((base + 3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((base + 3, 0)).label("$+$", "right")
     d += elm.Line().at((base + 3, 0)).to((base + 0, 0))
 
-    d += elm.SourceI().at((base + 0, -4)).to((base + 0, -0)).label("i[S]")
+    d += elm.SourceI().at((base + 0, -4)).to((base + 0, -0)).label("$i_S$")
 
     d += elm.Line().at((base + 0, -4)).to((base + 3, -4))
-    d += elm.Dot(open=True).at((base + 3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((base + 3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("i", "top")
+    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("$i$", "top")
 
     d += (
         elm.Line()
@@ -312,7 +312,7 @@ with Drawing(show=False) as d:
         .linewidth(0.5)
     )
 
-    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("u", "center")
+    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("$u$", "center")
 
     d.save(OUTPUT_DIR / "4_5_4.svg")
 
@@ -320,21 +320,21 @@ with Drawing(show=False) as d:
 with Drawing(show=False) as d:
     d.config(fontsize=STYLE["fontsize"])
 
-    d += elm.Dot(open=True).at((5, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((5, 0)).label("$+$", "right")
     d += elm.Line().at((5, 0)).to((0, 0))
 
-    d += elm.SourceV().at((0, -4)).to((0, 0)).label("u[S]")
+    d += elm.SourceV().at((0, -4)).to((0, 0)).label("$u_S$")
 
     d += elm.Dot(open=False).at((2, 0))
     d += elm.Line().at((2, 0)).to((2, -1.5))
-    d += Box(w=1, h=1).at((2, -1.5)).label("N′")
+    d += Box(w=1, h=1).at((2, -1.5)).label("$N′$")
     d += elm.Line().at((2, -2.5)).to((2, -4))
     d += elm.Dot(open=False).at((2, -4))
 
     d += elm.Line().at((0, -4)).to((5, -4))
-    d += elm.Dot(open=True).at((5, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((5, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((3, 0)).to((4, 0)).label("i", "top")
+    d += elm.Arrow().at((3, 0)).to((4, 0)).label("$i$", "top")
 
     d += (
         elm.Line().at((-1.5, +0.5)).to((+3.0, +0.5)).linestyle("dashdot").linewidth(0.5)
@@ -349,19 +349,19 @@ with Drawing(show=False) as d:
         elm.Line().at((-1.5, -4.5)).to((-1.5, +0.5)).linestyle("dashdot").linewidth(0.5)
     )
 
-    d += elm.Line().at((5, -2)).to((5, -2)).label("u", "center")
+    d += elm.Line().at((5, -2)).to((5, -2)).label("$u$", "center")
 
     base = 8
 
-    d += elm.Dot(open=True).at((base + 3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((base + 3, 0)).label("$+$", "right")
     d += elm.Line().at((base + 3, 0)).to((base + 0, 0))
 
-    d += elm.SourceV().at((base + 0, -4)).to((base + 0, 0)).label("u[S]")
+    d += elm.SourceV().at((base + 0, -4)).to((base + 0, 0)).label("$u_S$")
 
     d += elm.Line().at((base + 0, -4)).to((base + 3, -4))
-    d += elm.Dot(open=True).at((base + 3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((base + 3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("i", "top")
+    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("$i$", "top")
 
     d += (
         elm.Line()
@@ -392,7 +392,7 @@ with Drawing(show=False) as d:
         .linewidth(0.5)
     )
 
-    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("u", "center")
+    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("$u$", "center")
 
     d.save(OUTPUT_DIR / "4_5_5.svg")
 
@@ -400,18 +400,18 @@ with Drawing(show=False) as d:
 with Drawing(show=False) as d:
     d.config(fontsize=STYLE["fontsize"])
 
-    d += elm.Dot(open=True).at((3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((3, 0)).label("$+$", "right")
     d += elm.Line().at((3, 0)).to((0, 0))
 
-    d += elm.SourceI().at((0, -2)).to((0, 0)).label("i[S1]")
+    d += elm.SourceI().at((0, -2)).to((0, 0)).label("$i_S$")
     d += elm.Line().at((0, -2)).to((0, -2.5))
-    d += Box(w=1, h=1).at((0, -2.5)).label("N′")
+    d += Box(w=1, h=1).at((0, -2.5)).label("$N′$")
     d += elm.Line().at((0, -3.5)).to((0, -4))
 
     d += elm.Line().at((0, -4)).to((3, -4))
-    d += elm.Dot(open=True).at((3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((1, 0)).to((2, 0)).label("i", "top")
+    d += elm.Arrow().at((1, 0)).to((2, 0)).label("$i$", "top")
 
     d += (
         elm.Line().at((-1.5, +0.5)).to((+1.0, +0.5)).linestyle("dashdot").linewidth(0.5)
@@ -426,19 +426,19 @@ with Drawing(show=False) as d:
         elm.Line().at((-1.5, -4.5)).to((-1.5, +0.5)).linestyle("dashdot").linewidth(0.5)
     )
 
-    d += elm.Line().at((3, -2)).to((3, -2)).label("u", "center")
+    d += elm.Line().at((3, -2)).to((3, -2)).label("$u$", "center")
 
     base = 6
 
-    d += elm.Dot(open=True).at((base + 3, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((base + 3, 0)).label("$+$", "right")
     d += elm.Line().at((base + 3, 0)).to((base + 0, 0))
 
-    d += elm.SourceI().at((base + 0, -4)).to((base + 0, 0)).label("i[S]")
+    d += elm.SourceI().at((base + 0, -4)).to((base + 0, 0)).label("$i_S$")
 
     d += elm.Line().at((base + 0, -4)).to((base + 3, -4))
-    d += elm.Dot(open=True).at((base + 3, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((base + 3, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("i", "top")
+    d += elm.Arrow().at((base + 1, 0)).to((base + 2, 0)).label("$i$", "top")
 
     d += (
         elm.Line()
@@ -469,7 +469,7 @@ with Drawing(show=False) as d:
         .linewidth(0.5)
     )
 
-    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("u", "center")
+    d += elm.Line().at((base + 3, -2)).to((base + 3, -2)).label("$u$", "center")
 
     d.save(OUTPUT_DIR / "4_5_6.svg")
 
@@ -477,36 +477,36 @@ with Drawing(show=False) as d:
 with Drawing(show=False) as d:
     d.config(fontsize=STYLE["fontsize"])
 
-    d += elm.Dot(open=True).at((4.5, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((4.5, 0)).label("$+$", "right")
     d += elm.Line().at((3, 0)).to((4.5, 0))
-    d += elm.Resistor().at((1, 0)).to((3, 0)).label("R", "bottom")
+    d += elm.Resistor().at((1, 0)).to((3, 0)).label("$R$", "bottom")
     d += elm.Line().at((0, 0)).to((1, 0))
 
-    d += elm.SourceV().at((0, -4)).to((0, 0)).label("u[S]")
+    d += elm.SourceV().at((0, -4)).to((0, 0)).label("$u_S$")
 
     d += elm.Line().at((0, -4)).to((4.5, -4))
-    d += elm.Dot(open=True).at((4.5, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((4.5, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((3, 0)).to((3.5, 0)).label("i", "top")
+    d += elm.Arrow().at((3, 0)).to((3.5, 0)).label("$i$", "top")
 
-    d += elm.Line().at((4.5, -2)).to((4.5, -2)).label("u", "center")
+    d += elm.Line().at((4.5, -2)).to((4.5, -2)).label("$u$", "center")
 
     base = 8
 
-    d += elm.Dot(open=True).at((base + 5, 0)).label("+", "right")
+    d += elm.Dot(open=True).at((base + 5, 0)).label("$+$", "right")
     d += elm.Line().at((base + 5, 0)).to((base + 0, 0))
 
-    d += elm.SourceI().at((base + 0, -4)).to((base + 0, 0)).label("i[S]")
+    d += elm.SourceI().at((base + 0, -4)).to((base + 0, 0)).label("$i_S$")
 
     d += elm.Dot(open=False).at((base + 2, 0))
-    d += elm.Resistor().at((base + 2, 0)).to((base + 2, -4)).label("R′", loc="bottom")
+    d += elm.Resistor().at((base + 2, 0)).to((base + 2, -4)).label("$R′$", loc="bottom")
     d += elm.Dot(open=False).at((base + 2, -4))
 
     d += elm.Line().at((base + 0, -4)).to((base + 5, -4))
-    d += elm.Dot(open=True).at((base + 5, -4)).label("-", "right")
+    d += elm.Dot(open=True).at((base + 5, -4)).label("$-$", "right")
 
-    d += elm.Arrow().at((base + 3, 0)).to((base + 4, 0)).label("i", "right")
+    d += elm.Arrow().at((base + 3, 0)).to((base + 4, 0)).label("$i$", "right")
 
-    d += elm.Line().at((base + 5, -2)).to((base + 5, -2)).label("u", "center")
+    d += elm.Line().at((base + 5, -2)).to((base + 5, -2)).label("$u$", "center")
 
     d.save(OUTPUT_DIR / "4_5_7.svg")
